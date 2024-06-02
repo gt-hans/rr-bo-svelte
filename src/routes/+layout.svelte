7d0e6a3fe37afea25c1ca2df5446db5a74d284b6
hanswong <166590402+rr-hans@users.noreply.github.com>
2024-06-02 19:34:52 +0800

    init sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script>
 	import '../app.css';
 	import Navbar from '$lib/widgets/Navbar.svelte';
+  import Sidebar from '$lib/widgets/Sidebar.svelte';
+	let drawerHidden = false;
 </script>
 
-<Navbar/>
-<slot></slot>
+<header
+	class="fixed top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800"
+>
+	<Navbar bind:drawerHidden />
+</header>
+<div class="overflow-hidden lg:flex">
+	<Sidebar bind:drawerHidden />
+	<div class="relative h-full w-full overflow-y-auto lg:ml-64 pt-[70px]">
+		<slot />
+		
+	</div>
+</div>
 
 <style></style>

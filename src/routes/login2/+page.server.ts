ff0969a5782d6ec58eaf0e6b1cdc5d7ac5ffeb1c
hanswong <166590402+rr-hans@users.noreply.github.com>
2024-06-04 00:50:55 +0800

    modify login2

---
+++ src/routes/login2/+page.server.ts
@@ -0,0 +1,33 @@
+import { redirect } from '@sveltejs/kit';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ cookies, locals, params }) {
+  console.log('login2/+page.server.js load', cookies, locals, params);
+  let id = cookies.get('userid');
+
+  if (!id) {
+    id = crypto.randomUUID();
+    cookies.set('userid', id, { path: '/' });
+    return {
+      id
+    };
+  }
+}
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+  default: async ({ cookies, request }) => {
+    // TODO log the user in
+    const id = cookies.get('userid');
+    const data = await request.formData();
+    const email = data.get('email');
+    const password = data.get('password');
+    const result = { success: true, email, id };
+    console.log('login2/+page.server.js - action: login', result);
+    // return result;
+    // Delay the response by 1 second
+    return new Promise(resolve => setTimeout(() => {
+      resolve(result);
+    }, 2000));
+  }
+};
--- src/routes/login2/+page.svelte
+++ src/routes/login2/+page.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
-	import { Label, Input, Checkbox, A, Button, Card } from 'flowbite-svelte';
+  import { Label, Input, Checkbox, A, Button, Card } from 'flowbite-svelte';
+  import type { PageData, ActionData } from './$types';
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+  import { enhance } from '$app/forms';
+
+  export let form: ActionData;
+  export let data: PageData;
+  onMount(() => {
+    console.log('form:', form);
+    if (data?.id) {
+      console.log('data.id=', data.id);
+    }
+    if (form?.success) {
+      localStorage.setItem('user', JSON.stringify(form));
+      goto('/');
+    }
+  });
 </script>
 
-<form method="POST" action="?/login">
-  <label>
-    Email
-    <input name="email" type="email" />
-  </label>
-  <label>
-    Password
-    <input name="password" type="password" />
-  </label>
-  <Button type="submit" size="lg">Log In</Button>
-</form>
+{#if form?.success}
+  <Card>
+    <p>Logged in successfully</p>
+  </Card>
+{:else}
+  <form method="POST">
+    <label>
+      Email
+      <input name="email" type="email" />
+    </label>
+    <label>
+      Password
+      <input name="password" type="password" />
+    </label>
+    <Button type="submit" size="lg">Log In</Button>
+  </form>
+{/if}

f2630954a3c7387191aed2605b6aec36222ad2e8
Hans Wong <166590402+rr-hans@users.noreply.github.com>
2024-06-03 17:42:48 +0800

    pnpm install -D @vavite/node-loader and added debug profiles

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { nodeLoaderPlugin } from '@vavite/node-loader/plugin';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+export default defineConfig(({ mode }) => {
+  let plugins = [sveltekit()];
+  if (mode === 'development') {
+    plugins = [nodeLoaderPlugin(), ...plugins];
+  }
+  return {
+    // ... your code ...
+    plugins
+  };
 });
